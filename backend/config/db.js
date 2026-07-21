57dbe4a72109eab71c1370aaab34299fa46b08c7
Sandipta Bhattacharyya <sandiptabhattacharyya2345@gmail.com>
2026-07-22 04:20:45 +0530

    Move MongoDB URI to environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,16 @@
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
- export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://sandiptabhattacharyya1234_db_user:sandipta1234@cluster1.yjrnrwt.mongodb.net/FOOD-DEL').then(()=> console.log("DB connected"))
-}
+//  export const connectDB = async()=>{
+//     await mongoose.connect('mongodb+srv://sandiptabhattacharyya1234_db_user:sandipta1234@cluster1.yjrnrwt.mongodb.net/FOOD-DEL').then(()=> console.log("DB connected"))
+// }
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected");
+  } catch (error) {
+    console.log(error);
+  }
+};
